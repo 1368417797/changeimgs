d1824ca18f765945171c547daf6ecab2bc085a2b
1368417797 <1368417797@qq.com>
2020-01-16 01:10:00 +0800

    注释

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-setInterval(next,2000)//计时器调用数据
+setInterval(next,2000)//计时器调用下一张函数
 
 var n=0;
 
 var p=0;
-
+//切换图片的俩变量
 var flag=1;
-
+//鼠标事件的标志变量
 var img=document.querySelectorAll("img");
 
 img.forEach(element => {
@@ -14,15 +14,13 @@ img.forEach(element => {
     n=n+1;
 
 })
-
+//设定每张图的id，如需加图直接加然后class=“img”即可
 var num=n;
-
+//记录img数便于之后取余运算
 n=1;
 
 document.getElementById("img0").style.display = "block";
-
-document.getElementById("next").addEventListener('click',next);
-document.getElementById("last").addEventListener('click',last);
+//初始化n和第一张图
 
 document.getElementById("img").addEventListener('mouseover',change1)
 
@@ -37,7 +35,10 @@ function change2()
 {
     flag=1;
 }
-console.log(num)
+//鼠标事件
+document.getElementById("next").addEventListener('click',next);
+document.getElementById("last").addEventListener('click',last);
+
 function next()
 {
     if(flag == 2)
@@ -83,4 +84,4 @@ function last()
     document.getElementById([img2]).style.display = "none";
 
 }
-
+//两个函数下一张和上一张
